52a6b04cba0ce11b7b25d2aa63816809bf2ea399
Markus Albrecht <maralb@pixel.de>
2018-12-30 08:51:22 +0100

    day05 kotlin improvements

--- src/aoc18/day05/Day05.kt
+++ src/aoc18/day05/Day05.kt
@@ -1,71 +1,44 @@
+import java.io.File
+
 object Day05 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        part1()
+        part2()
+    }
+
+    private fun part1() {
+        val polymer = parseInput()
+        val reduced = react(polymer)
+        println("Part1: Unit count: ${reduced.size}")
+    }
+
+    private fun part2() {
+        val polymer = parseInput()
+        val shortest = ('a'..'z').map { c -> react(polymer, c).size }.min()
+        println("Part2: Shortest polymer: $shortest")
+    }
+
+    private fun react(polymer: String, ignore: Char? = null): List<Char> =
+        polymer.fold(mutableListOf<Char>()) { reduced, char ->
+            when {
+                ignore != null && char.equals(ignore, true) -> Unit
+                canReact(reduced.lastOrNull(), char) -> reduced.removeAt(reduced.size - 1)
+                else -> reduced.add(char)
+            }
+            reduced
+        }
 
     private var charDiff = 'a'.toInt() - 'A'.toInt()
 
-    @Throws(Exception::class)
-    @JvmStatic
-    fun main(args: Array<String>) {
-        teil1()
-        teil2()
+    private fun canReact(ch1: Char?, ch2: Char?): Boolean {
+        if (ch1 == null || ch2 == null) {
+            return false
+        }
+        return Math.abs(ch1 - ch2) == charDiff
     }
 
-    @Throws(Exception::class)
-    private fun teil2() {
-
-        this.javaClass.getResourceAsStream("aoc18/day05/input.txt")
-            .bufferedReader().forEachLine {
-
-            var minCount = Int.MAX_VALUE
-            for (ch in 'a'.toInt()..'z'.toInt()) {
-                var input = it.filter { c -> (c.toInt() != ch)}.filter { c -> (c.toInt() != (ch - charDiff)) }
-                var currInput = ""
-                var nextInput = input
-                while (!currInput.equals(nextInput)) {
-                    currInput = nextInput
-                    nextInput = reactRec(currInput)
-                    //println("=> $currInput / $nextInput")
-                }
-                //println("Char count: ${nextInput.length}" )
-                minCount = Math.min(minCount, nextInput.length)
-            }
-            println("Min count: $minCount")
-        }
-    }
-
-    @Throws(Exception::class)
-    private fun teil1() {
-
-        this.javaClass.getResourceAsStream("aoc18/day05/input.txt")
-            .bufferedReader().forEachLine {
-            var currInput = ""
-            var nextInput = it
-            while (!currInput.equals(nextInput)) {
-                currInput = nextInput
-                nextInput = reactRec(currInput)
-                //println("=> $currInput / $nextInput")
-            }
-            println("Char count: ${nextInput.length}" )
-        }
-    }
-
-    private fun reactRec(input: String): String {
-        var nextInput = StringBuilder()
-
-        var lastChar: Int? = null
-        for (ch in input.chars()) {
-            if (lastChar != null && Math.abs(lastChar - ch) == charDiff) {
-                lastChar = null
-            } else {
-                if (lastChar != null) {
-                    nextInput.append(lastChar.toChar())
-                }
-                lastChar = ch
-            }
-        }
-        if (lastChar != null) {
-            nextInput.append(lastChar.toChar())
-        }
-        return nextInput.toString()
-    }
-
+    private fun parseInput(): String =
+        File(this.javaClass.getResource("aoc18/day05/input.txt").toURI()).readText()
 }
