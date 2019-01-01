73c7a243037bf61c7e9a6fa07ea0f81b71546c5c
Markus Albrecht <maralb@pixel.de>
2019-01-01 19:49:57 +0100

    day09 kotlin improvements

--- src/aoc18/day09/Day09.kt
+++ src/aoc18/day09/Day09.kt
@@ -1,49 +1,42 @@
-import java.util.*
+import java.io.File
 
 object Day09 {
 
-    @Throws(Exception::class)
     @JvmStatic
     fun main(args: Array<String>) {
-        teil1u2()
+        part1and2()
     }
 
-    @Throws(Exception::class)
-    private fun teil1u2() {
+    private fun part1and2() {
+        val input = parseInput()
+        val players = Regex("^([0-9]+) ").find(input)!!.groups[1]!!.value
+        val marbles = Regex("worth ([0-9]+) points").find(input)!!.groups[1]!!.value
 
-        this.javaClass.getResourceAsStream("aoc18/day09/input.txt")
-            .bufferedReader().forEachLine {
-                val players = Regex("^([0-9]+) ").find(it)!!.groups[1]!!.value
-                val marbles = Regex("worth ([0-9]+) points").find(it)!!.groups[1]!!.value
-
-                play(players.toInt(), marbles.toInt())
-        }
+        play(players.toInt(), marbles.toInt())
+        play(players.toInt(), marbles.toInt() * 100)
     }
 
     private fun play(players: Int, marbles: Int) {
-        print("players: $players - marbles: $marbles")
-
         val scores = LongArray(players)
         val circle = Circle()
 
         for (marble in 1..marbles) {
-            var player = ((marble-1) % players) + 1
+            val player = ((marble - 1) % players) + 1
             if (marble % 23 == 0) {
                 val rmVal = circle.prev(7).remove()
-                scores[player-1] += marble.toLong() + rmVal.toLong()
+                scores[player - 1] += marble.toLong() + rmVal.toLong()
             } else {
                 circle.next().insertAfter(Element(marble))
             }
             //println("circle: $circle")
         }
-        println(" => Max score: ${scores.max()}")
+        println("Part:  marbles: $marbles - max score: ${scores.max()}")
     }
 
-    class Element(val value: Int, var next: Element? = null, var prev: Element? = null) {
-        override fun toString(): String {
-            return "[$value]"
-        }
-    }
+    private fun parseInput(): String =
+        File(this.javaClass.getResource("aoc18/day09/input.txt").toURI()).readText()
+
+    data class Element(val value: Int, var next: Element? = null, var prev: Element? = null)
 
     class Circle {
 
@@ -87,7 +80,7 @@ object Day09 {
             val startEl = curr;
             var el = curr;
 
-            var str  = "["
+            var str = "["
             do {
                 str += "$el "
                 el = el.next!!
