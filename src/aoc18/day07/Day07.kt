ae78a3c06d5a766f42bf9cab1947921af7261979
Markus Albrecht <maralb@pixel.de>
2019-01-01 17:44:29 +0100

    day07 kotlin improvements

--- src/aoc18/day07/Day07.kt
+++ src/aoc18/day07/Day07.kt
@@ -1,133 +1,106 @@
 object Day07 {
 
-    @Throws(Exception::class)
     @JvmStatic
     fun main(args: Array<String>) {
-        teil1()
-        teil2()
+        part1()
+        part2()
     }
 
-    class Node(val name: String, var duration: Int = 0, val toNodes: ArrayList<String> = ArrayList(), val fromNodes: ArrayList<String> = ArrayList()) {
-        override fun toString(): String {
-            return name
+    private fun part1() {
+        val nodes = parseInput()
+        val openNodes =
+            nodes.filter { n1 -> (nodes.find { n2 -> (n2.toNodes.contains(n1.name)) }) == null }.toMutableList()
+
+        print("Part1: ")
+        val doneNodes = mutableListOf<Char>()
+        while (!openNodes.isEmpty()) {
+            openNodes.sortBy { it.name }
+
+            val nextNode = openNodes.find { on -> (on.fromNodes.subtract(doneNodes).isEmpty()) }!!
+            openNodes.remove(nextNode)
+            doneNodes.add(nextNode.name)
+
+            print(nextNode.name)
+
+            openNodes.addAll(
+                nextNode.toNodes
+                    .map { toNodeName -> (nodes.find { n -> (n.name == toNodeName) })!! }
+                    .filter { n -> (!openNodes.contains(n)) })
         }
+        println()
     }
 
-    @Throws(Exception::class)
-    private fun teil2() {
-        val durationOffset = 61
+    private fun part2() {
         val workerCount = 5
 
-        val nodes = ArrayList<Node>()
+        val nodes = parseInput()
+        val openNodes =
+            nodes.filter { n1 -> (nodes.find { n2 -> (n2.toNodes.contains(n1.name)) }) == null }.toMutableList()
 
-        this.javaClass.getResourceAsStream("aoc18/day07/input.txt")
-            .bufferedReader().forEachLine {
-            val nodeFrom = it[5].toString()
-            val nodeTo = it[36].toString()
-
-            var nodesFrom = nodes.find { node ->  node.name.equals(nodeFrom) }
-            if (nodesFrom == null) {
-                nodesFrom = Node(nodeFrom, nodeFrom[0].toInt() - 'A'.toInt() + durationOffset)
-                nodes.add(nodesFrom)
-            }
-            nodesFrom.toNodes.add(nodeTo)
-
-            var nodesTo = nodes.find { node ->  node.name.equals(nodeTo) }
-            if (nodesTo == null) {
-                nodesTo = Node(nodeTo, nodeTo[0].toInt() - 'A'.toInt() + durationOffset)
-                nodes.add(nodesTo)
-            }
-            nodesTo.fromNodes.add(nodeFrom)
-        }
-
-        val openNodes: ArrayList<Node> = ArrayList()
-        openNodes.addAll(nodes.filter { n1 -> (nodes.find { n2 -> (n2.toNodes.contains(n1.name)) }) == null })
-
-        println("Start nodes: $openNodes")
         print("Result: ")
-
-        val workers = IntArray(workerCount, { 0 })
-        val workerNodes = Array<Node?>(workerCount, { null })
+        val workers = IntArray(workerCount) { 0 }
+        val workerNodes = Array<Node?>(workerCount) { null }
         var currSec = 0
-        val doneNodes = ArrayList<String>()
-        while (doneNodes.size < nodes.size || workerNodes.find { n ->  (n != null)} != null) {
-            println("sec: $currSec")
-            for (workerEndIdx in  workers.indices.filter { wIdx -> (workers[wIdx] == currSec) }) {
+        val doneNodes = mutableListOf<Char>()
+        while (doneNodes.size < nodes.size || workerNodes.any { it != null }) {
+
+            for (workerEndIdx in workers.indices.filter { wIdx -> (workers[wIdx] == currSec) }) {
                 if (workerNodes[workerEndIdx] != null) {
-                    println("worker end  : $workerEndIdx --> ${workerNodes[workerEndIdx]!!.name}")
+                    //println("worker end  : $workerEndIdx --> ${workerNodes[workerEndIdx]!!.name}")
                     doneNodes.add(workerNodes[workerEndIdx]!!.name)
                     openNodes.addAll(
-                            workerNodes[workerEndIdx]!!.toNodes
-                                    .map { toNodeName -> (nodes.find { n -> (n.name.equals(toNodeName)) })!! }
-                                    .filter { n -> (!openNodes.contains(n)) })
+                        workerNodes[workerEndIdx]!!.toNodes
+                            .map { toNodeName -> (nodes.find { n -> (n.name == toNodeName) })!! }
+                            .filter { n -> (!openNodes.contains(n)) })
                     workerNodes[workerEndIdx] = null
                 }
             }
 
             for (workerIdx in workers.indices.filter { wIdx -> (workers[wIdx] <= currSec) }) {
                 openNodes.sortBy { it.name }
-
                 val nextNode = openNodes.find { on -> (on.fromNodes.subtract(doneNodes).isEmpty()) }
                 if (nextNode != null) {
                     openNodes.remove(nextNode)
-
-                    println("worker start: $workerIdx ==> ${nextNode.name}")
-
+                    //println("worker start: $workerIdx ==> ${nextNode.name}")
                     workerNodes[workerIdx] = nextNode
                     workers[workerIdx] = currSec + nextNode.duration
                 }
             }
             currSec++
         }
-        println("Duration: ${currSec-1}")
+        println("Part2: duration: ${currSec - 1}")
     }
 
-    @Throws(Exception::class)
-    private fun teil1() {
-
-        val nodes = ArrayList<Node>()
+    private fun parseInput(): List<Node> {
+        val nodes = mutableListOf<Node>()
 
         this.javaClass.getResourceAsStream("aoc18/day07/input.txt")
             .bufferedReader().forEachLine {
-            val nodeFrom = it[5].toString()
-            val nodeTo = it[36].toString()
+                val nodeFrom = it[5]
+                val nodeTo = it[36]
 
-            var nodesFrom = nodes.find { node ->  node.name.equals(nodeFrom) }
-            if (nodesFrom == null) {
-                nodesFrom = Node(nodeFrom)
-                nodes.add(nodesFrom)
+                var nodesFrom = nodes.find { node -> node.name == nodeFrom }
+                if (nodesFrom == null) {
+                    nodesFrom = Node(nodeFrom)
+                    nodes.add(nodesFrom)
+                }
+                nodesFrom.toNodes.add(nodeTo)
+
+                var nodesTo = nodes.find { node -> node.name == nodeTo }
+                if (nodesTo == null) {
+                    nodesTo = Node(nodeTo)
+                    nodes.add(nodesTo)
+                }
+                nodesTo.fromNodes.add(nodeFrom)
             }
-            nodesFrom.toNodes.add(nodeTo)
-
-            var nodesTo = nodes.find { node ->  node.name.equals(nodeTo) }
-            if (nodesTo == null) {
-                nodesTo = Node(nodeTo)
-                nodes.add(nodesTo)
-            }
-            nodesTo.fromNodes.add(nodeFrom)
-        }
-
-        var openNodes: ArrayList<Node> = ArrayList()
-        openNodes.addAll(nodes.filter { n1 -> (nodes.find { n2 -> (n2.toNodes.contains(n1.name)) }) == null })
-
-        println("Start nodes: $openNodes")
-        print("Result: ")
-        var doneNodes = ArrayList<String>()
-        while (!openNodes.isEmpty()) {
-            openNodes.sortBy { it.name }
-
-            val nextNode = openNodes.find { on -> (on.fromNodes.subtract(doneNodes).isEmpty()) }
-            openNodes.remove(nextNode)
-            doneNodes.add(nextNode!!.name)
-
-            print("${nextNode.name}")
-
-            openNodes.addAll(
-                    nextNode.toNodes
-                            .map { toNodeName -> (nodes.find { n -> (n.name.equals(toNodeName)) })!! }
-                            .filter { n -> (!openNodes.contains(n)) })
-        }
-        println()
+        return nodes
     }
 
+    data class Node(
+        val name: Char,
+        val toNodes: MutableList<Char> = mutableListOf(),
+        val fromNodes: MutableList<Char> = mutableListOf()
+    ) {
+        val duration = name.toInt() - 'A'.toInt() + 61
+    }
 }
